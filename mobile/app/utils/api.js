8a038df2461a49e8930c92721b2788aca9bc1b49
Rafael Dwiky <rafaeltamvan10@gmail.com>
2025-12-03 20:16:29 +0700

    fix

--- mobile/app/utils/api.js
+++ mobile/app/utils/api.js
@@ -1,11 +1,12 @@
 // Jika HP fisik: ganti ke IP laptop kamu, contoh:
-// const API_URL = "http://192.168.0.107:8000"; //! Kos Ihya // sesuaikan dengan IP address laptop kamu
-// const API_URL = "http://10.10.180.188:8000"; //! Wifi Teras // sesuaikan dengan IP address laptop kamu
+// const API_URL = "http://192.168.0.112:8000"; //! Kos Ihya // sesuaikan dengan IP address laptop kamu
+const API_URL = "http://10.10.180.232:8000"; //! Wifi Teras // sesuaikan dengan IP address laptop kamu
 // const API_URL = "http://10.10.5.154:8000"; //! Wifi RPL // sesuaikan dengan IP address laptop kamu
 // const API_URL = "http://192.168.1.108:8000"; //! Wifi 575 // sesuaikan dengan IP address laptop kamu
 // const API_URL = "http://192.168.137.1:8000"; //! LAN KSI // sesuaikan dengan IP address laptop kamu
 // const API_URL = "http://10.1.0.74:8000"; //! Wifi Cakwang // sesuaikan dengan IP address laptop kamu
-const API_URL = "http://10.10.180.251:8000"; //! Wifi Teras // sesuaikan dengan IP address laptop kamu
+// const API_URL = "http://10.10.180.251:8000"; //! Wifi Teras // sesuaikan dengan IP address laptop kamu
+// const API_URL = "http://10.10.181.116:8000"; //! Wifi TI // sesuaikan dengan IP address laptop kamu
 // const API_URL = "http://192.168.1.8:8000"; //? Wifi Rumah // sesuaikan dengan IP address laptop kamu
 
 export default API_URL;
